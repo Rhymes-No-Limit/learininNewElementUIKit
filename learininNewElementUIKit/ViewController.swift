cd2aee1b99d46892c42cf70bd299baeceaa7695f
Andrey <Rhymes.un.limit@gmail.com>
2025-04-27 14:27:27 +0300

    Creating functionality keyboard

--- learininNewElementUIKit/ViewController.swift
+++ learininNewElementUIKit/ViewController.swift
@@ -5,6 +5,7 @@ class ViewController: UIViewController {
     @IBOutlet var countLabel: UILabel!
     @IBOutlet var textView: UITextView!
     
+    @IBOutlet var textViewBottomConstraints: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -13,9 +14,44 @@ class ViewController: UIViewController {
         textView.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 17)
         textView.backgroundColor = self.view.backgroundColor
         textView.layer.cornerRadius = 10
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateTextView(notification:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(updateTextView(notification:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        self.view.endEditing(true) // Скрытие клавиатуры для любого объекта
+        
+      //  textView.resignFirstResponder() // Скрытие клавиатуры для вызванной для конкретного объекта
+    }
+    
+    @objc func updateTextView(notification: Notification) {
+        
+        guard
+            let userInfo = notification.userInfo as? [String: Any],
+            let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        else { return }
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            textView.contentInset = UIEdgeInsets.zero
+        } else {
+            textView.contentInset = UIEdgeInsets(top: 0,
+                                                 left: 0,
+                                                 bottom: keyboardFrame.height - textViewBottomConstraints.constant,
+                                                 right: 0)
+            textView.scrollIndicatorInsets = textView.contentInset
+        }
+        
+        textView.scrollRangeToVisible(textView.selectedRange)
+    }
 }
 
 extension ViewController: UITextViewDelegate {
